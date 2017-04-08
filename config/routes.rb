0a912dcd5dec6a9389c40dc43b3c3b3675ecad53
Liang xinzhong <johnson.liangxinzhong@gmail.com>
2017-04-08 22:21:19 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       post 'upvote'
       post 'downvote'
     end
+    collection do
+      get 'about'
+    end
   end
   root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
